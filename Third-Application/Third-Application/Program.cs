a61d173f282f2c10da082883fba80b372bfd9366
sabiddin <abiddinsyed@gmail.com>
2018-02-28 15:24:50 -0500

    No Message

---
+++ Third-Application/Third-Application/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Third_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aap hai na mujhe bilkul hi pasand nahin aaye, bahut koshis ki");
+            Console.Read();
+        }
+    }
+}
