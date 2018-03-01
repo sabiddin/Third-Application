d4b9d74a624797552fdd90d3a8eb4e74e2c4e824
sabiddin <abiddinsyed@gmail.com>
2018-02-28 20:36:34 -0500

    step-2: Added database project

---
+++ Third-Application/School.Database/Tables/tbl_Course.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tbl_Course](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[CourseNumber] [varchar](50) NULL,
+	[CourseName] [varchar](50) NULL,
+	[CourseDescription] [varchar](50) NULL,
+	[Credits] [int] NULL,
+	[DepartmentID] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
